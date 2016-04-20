215f5ddd031000b8beebfb57174df1182af4d83f
gitrocious <mdelah02@seattlecentral.edu>
2016-04-20 15:36:39 -0700

    Added files via upload

---
+++ bookshelf.php
@@ -0,0 +1,15 @@
+<?php include ("includes/top.php");?>
+<section class="main">
+<article class="withaside">
+<h2><?=$myPageID?></h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis dolor interdum, rutrum justo eu, congue orci. Nulla commodo condimentum mauris, ut eleifend libero molestie in.</p>
+<p>Vivamus efficitur ullamcorper elit, at pretium leo tincidunt vitae. Maecenas sit amet lobortis felis. Aliquam id purus vitae odio luctus sagittis. Phasellus ultrices et libero aliquet lacinia. Proin finibus viverra pretium. In molestie enim non accumsan molestie. Ut porttitor dictum sagittis.</p>
+<p>In ac lacus tempor, viverra metus eget, elementum libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus nibh convallis tristique scelerisque. </p>
+</article>
+
+<aside>
+<?php include ("includes/follow.php");?>
+
+</aside>
+</section>
+<?php include ("includes/bottom.php");?>
---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php include ("includes/top.php");?>
+<section class="main">
+<article class="withaside">
+<h2><?=$myPageID?></h2>
+		<p>Nulla aliquet sollicitudin mi, eu faucibus ex ultrices vel.</p>
+		<div id="formcontainer">
+				<?php
+		include 'includes/contact-simple.php';
+	?>  
+		</div>
+</article>
+
+<aside>
+<?php include ("includes/follow.php");?>
+
+</aside>
+</section>
+<?php include ("includes/bottom.php");?>
