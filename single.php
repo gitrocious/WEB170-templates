cc0ff5a6dc4d90b1df1c1695eb794fc85cd5fccf
gitrocious <mdelah02@seattlecentral.edu>
2016-05-26 15:10:41 -0700

    Create & tag page templates
    Also tagged index.php

--- single.php
+++ single.php
@@ -11,7 +11,7 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis dolor interdum, rutrum justo eu, congue orci. Nulla commodo condimentum mauris, ut eleifend libero molestie in.</p>
     <p>Vivamus efficitur ullamcorper elit, at pretium leo tincidunt vitae. Maecenas sit amet lobortis felis. Aliquam id purus vitae odio luctus sagittis. Phasellus ultrices et libero aliquet lacinia. Proin finibus viverra pretium. In molestie enim non accumsan molestie. Ut porttitor dictum sagittis.</p>
     <p>In ac lacus tempor, viverra metus eget, elementum libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus nibh convallis tristique scelerisque. </p>
-<small>index.php</small>
+<small>single.php</small>
   </article>
 <?php get_sidebar(); ?>  
 <?php get_footer(); ?>
