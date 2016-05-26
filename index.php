58ba07fa38a4542b6fc075093b61d3a5200282cb
gitrocious <mdelah02@seattlecentral.edu>
2016-05-26 14:53:23 -0700

    Fixed content layout
    Adjusted to get blog feed into left column instead of across whole page

--- index.php
+++ index.php
@@ -64,14 +64,14 @@
 </header>
 
 <section class="main">
-  <div class="postings">
+
+   <article class="withaside">
+    <div class="postings">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //if there are posts, get the post(s) ?>
   <h2><a href="<?php the_permalink(); //link to the post in its orig location ?>"><?php the_title(); //get title of the post ?></a></h2>
   <?php the_content(''); //get content of the post ?>
   <?php endwhile; endif; //end the loop ?>
   </div>
-  
-  <article class="withaside">
     <h2>Books and Works in Progress</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis dolor interdum, rutrum justo eu, congue orci. Nulla commodo condimentum mauris, ut eleifend libero molestie in.</p>
     <p>Vivamus efficitur ullamcorper elit, at pretium leo tincidunt vitae. Maecenas sit amet lobortis felis. Aliquam id purus vitae odio luctus sagittis. Phasellus ultrices et libero aliquet lacinia. Proin finibus viverra pretium. In molestie enim non accumsan molestie. Ut porttitor dictum sagittis.</p>
